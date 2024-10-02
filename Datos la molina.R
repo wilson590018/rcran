36dda154811f355d6f5e235c46b6d43e07504991
unknown <wguerra@iiap.gob.pe>
2024-10-02 17:20:16 -0500

    mi primer repositorio

---
+++ Datos la molina.R
@@ -0,0 +1,10 @@
+library(readxl)
+fb <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
+                                            sheet = "fb")
+View(LA_MOLINA_2014_POTATO_WUE_FB_)
+
+str(LA_MOLINA_2014_POTATO_WUE_FB_)
+
+View(fb)
+
+str(fb)
